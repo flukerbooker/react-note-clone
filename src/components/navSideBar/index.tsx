6f7b78239a63e0109e328307e99c7d4cd6cb4bb4
flukerbooker <win.vorapol52@gmail.com>
2021-06-22 17:32:46 +0700

    [ADD] Mockup Data

--- src/components/navSideBar/index.tsx
+++ src/components/navSideBar/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './index.module.scss'
 import ListIcon from '@material-ui/icons/List'
+import NoteItem from '../noteItem/'
+// import data from '../noteItem/mockupItem'
 
 const NavSideBar: React.FC = () => {
+  // const [isToggle, setIsToggle] = useState(true)
+  // const [list, setList] = useState([])
+
+  useEffect(() => {
+    // setList(data)
+  }, [])
+
   return (
     <nav className={`${styles.sidebar}`}>
       <div className={`${styles.icon}`}>
         <ListIcon style={{ color: 'grey' }} />
       </div>
       <h5 className={`${styles.title}`}>Notes</h5>
+      <NoteItem />
     </nav>
   )
 }
---
+++ src/components/noteItem/mockupItem.tsx
@@ -0,0 +1,47 @@
+interface IListCards {
+  id: string
+  title: string
+  note: string
+  createdAt: string
+  updatedAt: string
+}
+
+const mockupListCards: IListCards[] = [
+  {
+    id: '1',
+    title: '1UP BOOTCAMP',
+    note: 'Day 1',
+    createdAt: '',
+    updatedAt: '2021-06-22T07:46:34.094Z',
+  },
+  {
+    id: '2',
+    title: '1UP BOOTCAMP',
+    note: 'Day 2',
+    createdAt: '',
+    updatedAt: '2021-07-22T07:46:34.094Z',
+  },
+  {
+    id: '3',
+    title: '3UP BOOTCAMP',
+    note: 'Day 3',
+    createdAt: '',
+    updatedAt: '2021-08-22T07:46:34.094Z',
+  },
+  {
+    id: '4',
+    title: '4UP BOOTCAMP',
+    note: 'Day 4',
+    createdAt: '',
+    updatedAt: '2021-09-22T07:46:34.094Z',
+  },
+  {
+    id: '5',
+    title: '5UP BOOTCAMP',
+    note: 'Day 5',
+    createdAt: '',
+    updatedAt: '2021-09-22T07:46:34.094Z',
+  },
+]
+
+export default mockupListCards
